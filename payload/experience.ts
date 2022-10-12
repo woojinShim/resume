f8674b23230e9d14a713031299af417a5bc75bf9
LewisShim <lewis@atomrigs.io>
2022-10-12 21:59:58 +0900

    -5

--- payload/experience.ts
+++ payload/experience.ts
@@ -11,10 +11,10 @@ const experience: IExperience.Payload = {
       endedAt: '2022-08',
       descriptions: [
         '블록체인 Secure MPC 기반 지갑, 디앱 개발 스타트업 (17명)',
-        'Studiearn NFT Contract Test Case, UI개발',
+        'Studiearn NFT Contract UI개발',
         'P2E 포트리스 아레나 NFT Marketplace Contract Test Case',
-        '간송미술관 NFT 보유자에게 영인본 배송을 위한 주소입력 서비스 개발',
-        '간송미술관 NFT 수요조사 설문지 페이지 개발',
+        '간송미술관 NFT 홀더 대상 영인본 배송 주소입력 서비스 개발',
+        '간송미술관 NFT 수요조사 지갑 기반 설문 개발',
       ],
       skillKeywords: [
         'Etherurm',
